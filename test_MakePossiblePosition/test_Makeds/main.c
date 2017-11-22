666a23f5f3839dcc5eca78005b6fd8dde978f732
Szkieletor37 <akitosano0111@gmail.com>
2017-11-22 10:53:43 +0900

    pointの定義を変えて、dictionary_specialtownへの挿入は成功した。これはtest_Makedsに入っている。あと、Makefileのマクロを試した。

---
+++ test_MakePossiblePosition/test_Makeds/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "dictionary_specialtown.h"
+#include "dictionary_building.h"
+
+int main() {
+
+	put_data_of_building_to_dictionary();
+	
+	put_data_of_specialtown_to_dictionary();
+
+	return 0;
+
+}
+
+
