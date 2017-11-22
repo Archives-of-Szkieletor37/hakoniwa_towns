36df1c69578187c71040f1f98f4d6be83017243f
Szkieletor37 <akitosano0111@gmail.com>
2017-11-22 09:28:28 +0900

    test_Makedbを新しいpointの定義で動くようにした

--- test_MakePossiblePosition/test_Makedb/building.c
+++ test_MakePossiblePosition/test_Makedb/building.c
@@ -7,14 +7,14 @@
 
 int list_B_length[11] = {0,1,2,3,4,5,6,7,8,9,10};
 
-Building building_null = {"null", point_init_zero, NULL, NULL, NULL};
+//Building building_null = {"null", point_zero, NULL, NULL, NULL};
 
 Building *building_init(char* name, Point* point, int length, int width) {
 
 	Building *B_object = malloc(sizeof(Building));
 
 	strcpy(B_object->B_name, name);
-	B_object->B_left_bottom_point = point_init(point->x, point->y, point->flag);
+	B_object->B_left_bottom_point = point;
 	B_object->B_length = return_pointer_to_length(length);
 	B_object->B_width = return_pointer_to_length(width);
 
@@ -28,19 +28,19 @@ Building *building_init(char* name, Point* point, int length, int width) {
 	return B_object;
 
 }
-/*
+
 Building building_null(void) {
 	Building b;
 	
 	strcpy(b.B_name, "init");
-	b.B_left_bottom_point = point_init_zero();
+	b.B_left_bottom_point = &(point_zero);
 	b.B_length = NULL;
 	b.B_width = NULL;
 	b.B_rotate = NULL;
 
 	return b;
 }
-*/
+
 int *return_pointer_to_length(int B_length) {
 	
 	int *p = NULL;
